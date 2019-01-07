0eb0391b6b2af4cc1b38f79b52ecfe8ccedf1178
Serene Lim <Serene@serenes-mac.local>
2019-01-07 15:17:02 +0800

    added script to make menu working

--- views/components/sidemenu.jsx
+++ views/components/sidemenu.jsx
@@ -2,33 +2,11 @@ var React = require('react');
 
 class SideMenu extends React.Component {
     render() {
-        var menu = document.querySelector(".menu"),
-        toggle = document.querySelector(".menu-toggle");
-
-        function toggleToggle() {
-          toggle.classList.toggle("menu-open");
-        };
-
-        function toggleMenu() {
-          menu.classList.toggle("active");
-        };
-
-        toggle.addEventListener("click", toggleToggle, false);
-        toggle.addEventListener("click", toggleMenu, false);
 
         return (
-            <nav role="navigation">
-              <div class="menu-toggle"><span>Menu</span></div>
-              <ul class="menu">
-                <li><a href="#"><span>Home</span></a></li>
-                <li><a href="#"><span>Learn More</span></a></li>
-                <li><a href="#"><span>Get Involved</span></a></li>
-                <li><a href="#"><span>Have Some Fun</span></a></li>
-                <li><a href="#"><span>Don't Miss Out</span></a></li>
-                <li><a href="#"><span>Get The Gear</span></a></li>
-              </ul>
-            </nav>
-        );
+          <h1>hi</h1>
+
+        )
     }
 };
 
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,23 +4,38 @@ var SideMenu = require('../components/sidemenu');
 // import { SideMenu } from "../components/sidemenu";
 
 class DefaultLayout extends React.Component {
-  render() {
+render() {
 
-    return (
-        <html>
-            <head>
-                <title>{this.props.title}</title>
-                <link rel="stylesheet" type="style/css" href="/style.css"/>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
-            </head>
-            <body>
-            <SideMenu/>
-            {this.props.children}
-            </body>
-        </html>
-    );
+return (
+    <html>
 
-  }
+    <head>
+        <title>{this.props.title}</title>
+        <link rel="stylesheet" type="text/css" href="/style.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            crossorigin="anonymous" />
+    </head>
+
+    <body>
+        <a href="#menu" id="toggle"><span></span></a>
+
+        <div id="menu">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+
+        <script type="text/javascript" src="/script.js"/>
+        <SideMenu />
+        {this.props.children}
+    </body>
+
+    </html>
+);
+
+}
 }
 
 module.exports = DefaultLayout;
