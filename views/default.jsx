d256b9ce0290917322e23751e790c13f21995154
mrandmrspoh <elainel.atwork@gmail.com>
2019-01-07 15:43:41 +0800

    mobile responsive header

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,7 @@ class Default extends React.Component{
         return (
             <React.Fragment>
                 <head>
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+                    <meta className="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
                     <link rel="stylesheet" type="text/css" href="/style.css"/>
                     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"/>
@@ -15,16 +15,28 @@ class Default extends React.Component{
                     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
                 </head>
                 <header>
-                    <img src="https://images.vexels.com/media/users/3/143221/isolated/preview/0654b09f8ecee586c3f9fe80d602e197-red-sea-animal-fish-cartoon-by-vexels.png"/>
-                    <h1>Creepylobster's Cookbook</h1>
-                    <ul>
-                        <a href='/recipes/'><li>Home</li></a>
-                        <a href='/recipes/new'><li>Create Recipe</li></a>
-                    </ul>
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <img src="https://images.vexels.com/media/users/3/143221/isolated/preview/0654b09f8ecee586c3f9fe80d602e197-red-sea-animal-fish-cartoon-by-vexels.png"/>
+                        <h1>Creepylobster's Cookbook</h1>
+                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                          </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <div class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href='/recipes/'>Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href='/recipes/new'>Create Recipe</a>
+                                </li>
+                            </div>
+                        </div>
+                    </nav>
                 </header>
                 <body>
                     {this.props.children}
                 </body>
+
             </React.Fragment>
         )
     }
