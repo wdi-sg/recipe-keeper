e3870e2a8f1f2fc982b5e2b89532762640559ac9
MARCYKAY <mahchinkok@gmail.com>
2019-07-19 19:38:18 +0800

    commit: with RESTfuls methods.

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ app.use( express.urlencoded( {
     extended: true
 } ) );
 
-const methodOverride = require( 'method-override' );
-app.use( methodOverride( '_method' ) );
-
 const reactEngine = require( 'express-react-views' ).createEngine();
 app.engine( 'jsx', reactEngine );
 app.set( 'views', __dirname + '/views' );
 app.set( 'view engine', 'jsx' );
 
+const methodOverride = require( 'method-override' );
+app.use( methodOverride( '_method' ) );
+
+
 
 app.get( '/', ( req, res ) => {
     res.render( 'home');
 } );
-
-
+// app.post( '/post', addRecipe );
+// app.put('/receipes/:id/edit', updateRecipe );
+// app.delete('/receipes/:id/delete', deleteReceipe);
 
 app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
