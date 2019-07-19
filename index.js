1ee3f02759fa6a9b6df650ff524a5f0042060449
yoda14 <choobthenoob@hotmail.com>
2019-07-19 23:54:44 +0800

    Added the first files, sends Get good Daniel!!

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log('In the beginning');
+const jsonfile = require('jsonfile');
+
+const file = 'data.json';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname+'/public/'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const methodOverride = require('method-override')
+
+app.use(methodOverride('_method'));
+
+const reactEngine = require('express-react-views').createEngine();
+
+app.engine('jsx', reactEngine);
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'jsx');
+
+console.log('Engine goes vroom!');
+
+app.get('/', (request, response) =>{
+    response.send("Get good Daniel!!");
+});
+
+
+app.listen(3000, () => console.log('~~~ Local host love you 3000 ~~~'));
