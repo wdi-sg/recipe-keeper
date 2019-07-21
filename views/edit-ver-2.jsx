d2fb8353613d80066f799ae503c2855d2c284117
gitwenlei <ngwenlei@gmail.com>
2019-07-22 00:57:05 +0800

    Updated input data names

--- views/edit-ver-2.jsx
+++ views/edit-ver-2.jsx
@@ -4,7 +4,9 @@ class Edit extends React.Component {
   render() {
     var url = "/recipes/" + this.props.userEnteredIdKey;
     var formAction = url + "?_method=PUT";
-    const ingredList = this.props.recipeData.ingredients[0];
+
+    const ingredList = this.props.recipeData.ingredients.join("\n");
+
     return (
       <html>
           <head>
@@ -26,15 +28,27 @@ class Edit extends React.Component {
                 </div>
                 <div className="col-8 card shadow p-3 mb-5 bg-white rounded">
                     <form action={formAction} method="POST">
-                        <p className="action-header">Recipe Id: </p>
-                        <input className="recipe-input-id" type="number" name="id" defaultValue={this.props.recipeData.id} />
+                        <div class="form-group">
+                            <p className="action-header">Recipe Id: </p>
+                            <input className="recipe-input-id" type="number" name="id" defaultValue={this.props.recipeData.id} />
 
-                        <p className="action-header">Edit Recipe Name: </p>
-                        <input className="recipe-input-name" name="name" defaultValue={this.props.recipeData.name} />
+                            <p className="action-header">Edit Recipe Name: </p>
+                            <input className="recipe-input" type="text" name="name" defaultValue={this.props.recipeData.name} />
 
-                        <p className="action-header">Edit Recipe Ingredients: </p>
-                        <textarea className="form-control" rows="3">{ingredList}</textarea>
-                        <input className="submit-input-btn" type="submit" value="Submit"/>
+                            <p className="action-header">Edit Recipe Ingredients: </p>
+                            <textarea name="ingredients" className="form-control" rows="8">{ingredList}</textarea>
+
+                            <p className="action-header">Edit Source Url: </p>
+                            <input className="recipe-input" type="text" name="source_url" defaultValue={this.props.recipeData.source_url} />
+
+                            <p className="action-header">Edit Recipe Id: </p>
+                            <input className="recipe-input" type="text" name="recipe_id" defaultValue={this.props.recipeData.recipe_id} />
+
+                            <p className="action-header">Edit Recipe Image Url: </p>
+                            <input className="recipe-input" type="text" name="image_url" defaultValue={this.props.recipeData.image_url} />
+
+                            <input className="submit-input-btn" type="submit" value="Submit"/>
+                        </div>
                     </form>
                 </div>
             </div>
