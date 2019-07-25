0584fdb4e9383ca4df58bfedb0c4deb606be1acc
marcykay <51184072+marcykay@users.noreply.github.com>
2019-07-25 16:13:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -207,4 +207,4 @@ app.get('/recipes', (req, res) => {
 
 
 
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+app.listen(80, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
