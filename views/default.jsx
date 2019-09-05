56ff88d95d9fde8f0dab49a142436d0a9b29932d
Wong Yat Sing Jason  (NCS) <yat.sing.jason.wong@ncs.com.sg>
2019-09-05 12:20:02 +0800

    change index.js file location

--- views/default.jsx
+++ views/default.jsx
@@ -5,11 +5,15 @@ class DefaultLayout extends React.Component {
     return (
       <html>
         <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
             <title>Recipe Keeper</title>
-            <link rel="stylesheet" href="/css/style.css" type="text/css" />
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
-            <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet"></link>
+            
         </head>
+        <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+            <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet" />
         
             <header>
                 <nav className="navbar navbar-expand-lg navbar-light bg-success">
