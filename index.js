d4e577f1237df7cad9221805003039e4b90b2624
lhjd <hongjinlau@gmail.com>
2019-10-12 00:04:57 +0800

    Initial setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const jsonfile = require("jsonfile");
+
+const file = "data.json";
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public/"));
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
+
+const reactEngine = require("express-react-views").createEngine();
+
+app.engine("jsx", reactEngine);
+
+app.set("views", __dirname + "/views");
+
+app.set("view engine", "jsx");
+
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
+app.listen(3000);
