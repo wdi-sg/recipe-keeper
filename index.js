662de1fd4ef36c66b7c5c6910368a66c9a2c00ce
dannyozh <danielongzhihao@gmail.com>
2019-10-13 14:42:46 +0800

    added portions for edit

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.post('/newrecipe', (request,response) => {
             all.recipes.push(newRecipe);
             let newRecipeDetails = {
                 title: request.body.title,
+                id: request.body.id,
                 ingredients: request.body.ingredients,
                 instructions: request.body.instructions
             }
@@ -62,11 +63,45 @@ app.post('/newrecipe', (request,response) => {
 
 // app.get for seeing all recipes
 app.get("/allrecipes", (req,res) => {
-
+    console.log("sifting through recipe book!")
     jsonfile.readFile(file, (err, obj) => {
         let recipes = obj.recipes;
         console.log(recipes);
         res.send(recipes);
+    });
+});
+
+// editing recipes
+app.get('/allrecipes/:id/edit', (req,res) => {
+    console.log("in editing page!")
+    let id = parseInt(req.params.id);
+    jsonfile.readFile(file, (err, obj) => {
+        console.log("searching for recipe!");
+        const data = {
+            id: obj.recipes[id-1].id,
+            title: obj.recipes[id-1].title,
+            ingredients: obj.recipes[id-1].ingredients,
+            instructions: obj.recipes[id-1].instructions
+        };
+        res.render('editform', data)
+    });
+});
+
+app.put('/allrecipes/:id', (req,res) => {
+    var recipeIndex = parseInt(req.params.id);
+    var editedRecipe = req.body;
+
+    jsonfile.readFile(file, (err, obj) => {
+        console.log("editing recipe!");
+
+        obj.recipes[recipeIndex-1] = editedRecipe;
+
+        console.log(editedRecipe);
+
+        jsonfile.writeFile(file, obj, (err) => {
+            console.log(err);
+            res.send("yay edited");
+        });
     });
 });
 
