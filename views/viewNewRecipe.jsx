c96e39379fc58a36a268bcf87ec3d489d86c8824
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-10-13 21:13:27 +0800

    updated the style of the page

--- views/viewNewRecipe.jsx
+++ views/viewNewRecipe.jsx
@@ -6,16 +6,21 @@ class ViewNew extends React.Component {
 
     return(
       <html>
+       <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet"></link>
+        </head>
         <body>
+        <main  style={{ margin: '0 auto',  width: '800px', fontFamily: 'Montserrat'}} >
 
-          <ul>
-             <li>id: {this.props.id}</li>
-            <li>title: {this.props.title}</li>
+            <h1 class="text-center" style={{ marginTop: '20px', }}>{this.props.title} </h1><br/>
+            <img width="600" height="auto" src={this.props.img}/>
 
-            <li><img width="400" height="auto" src={this.props.img}/></li>
-            <li>ingregients: {this.props.ingredients}</li>
-            <li>preparation: {this.props.preparation}</li>
-            </ul>
+            <h5>Ingregients: </h5> <p>{this.props.ingredients}</p>
+            <h5>Preparation: </h5> <p>{this.props.preparation}</p>
+
+            </main>
         </body>
       </html>
     );
