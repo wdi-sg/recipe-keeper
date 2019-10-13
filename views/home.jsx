7b3afe7732fb8568861572489370aba7c795f1cf
dannyozh <danielongzhihao@gmail.com>
2019-10-13 19:07:54 +0800

    added some css styling to home page

--- views/home.jsx
+++ views/home.jsx
@@ -13,14 +13,28 @@ class Home extends React.Component {
             <div class="container text-center">
             <h1>Food Food Food!</h1>
             <img class="float-left" src="https://media1.tenor.com/images/9f5ff2129ac255bfeea3d6ae81023ad2/tenor.gif?itemid=11225590"/>
-                <form method="POST" action="newrecipe">
+                <form method="POST" action="newrecipe" class="float-right">
                     <h1>Add a new recipe:</h1>
-                    <p>Recipe Title:</p>
-                    <input type="text" name="title"/><br/>
-                    <p>Required Ingredients:</p>
-                    <input type="text" name="ingredients"/><br/>
-                    <p>Instructions:</p>
-                    <input type="text" name="instructions"/><br/>
+                    <div class="row">
+
+                      <div class="col-md-6">
+                        <p>Recipe number:</p>
+                        <input type="num" name="id"/><br/><br/>
+                        <p>Recipe Title:</p>
+                        <input type="text" name="title"/><br/><br/>
+                        <p>How it should look like:</p>
+                        <input type="url" name="img"/><br/><br/>
+                      </div>
+
+                      <div class="col-md-6">
+                        <p>Recipe Type:</p>
+                        <input type="text" name="type" placeholder="bfast / lunch / dinner"/><br/><br/>
+                        <p>Required Ingredients:</p>
+                        <input type="text" name="ingredients"/><br/><br/>
+                        <p>Instructions:</p>
+                        <input type="text" name="instructions"/><br/><br/>
+                      </div>
+                    </div>
                     <input type="submit" value="Submit"/>
                 </form>
             </div>
