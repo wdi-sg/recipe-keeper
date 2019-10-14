26ea4b81f691f1e256a3acb30bb36d12e9c366a3
Sharon <sharxnrxse1312@gmail.com>
2019-10-14 09:24:44 +0800

    Deliverables done.

---
+++ views/new.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+class New extends React.Component {
+  render() {
+
+    let actionAttribute = '/recipes'
+
+    return (
+        <form method="POST" action={actionAttribute}>
+            Title:<input type="text" name="title"/><br/>
+            Ingredients:<input type="text" name="ingredients"/><br/>
+            Instructions:<input type="text" name="instructions"/><br/>
+            <input type="submit" value="Click to submit"/>
+        </form>
+
+    )
+  }
+}
