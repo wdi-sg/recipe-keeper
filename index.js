96d48e8e957e62f8f7519747ba621d1f4d079e93
Jess <jessicatanhc@gmail.com>
2020-01-17 16:11:26 +0800

    Add create new recipe

--- index.js
+++ index.js
@@ -17,6 +17,31 @@ app.engine('jsx', reactEngine);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 //End Of setting up
-
-
+//*********
+//Functions
+//*********
+const addRecipe = (request,response)=>{
+    console.log("creating new recipe");
+    jsonfile.readFile(file, (err, obj) => {
+     if( err ){
+       console.log("error with json read file:",err);
+       response.status(503).send("error reading file");
+       return;
+      }
+      const data = request.body;
+        obj.recipes.push(data);
+    jsonfile.writeFile(file , obj, (err) => {
+      console.error(err);
+    });
+        console.log("Done reading");
+        response.send("Success");
+    });
+}
+//*******
+//Routes
+//*******
+app.get('/recipes/new',(request,response)=>{
+    response.render('createPage');
+})
+app.post('/recipes', addRecipe)
 app.listen(3000, () => console.log("~~ Tuning in to port 3000 ~~"));
---
+++ views/createPage.jsx
@@ -0,0 +1,35 @@
+
+var React = require('react');
+var DefaultLayout = require('./layouts/default');
+
+class createPage extends React.Component {
+  render() {
+    return (
+   <DefaultLayout  title="New Recipe">
+<div className="container h-100">
+    <div className="row h-100 justify-content-center align-items-center">
+        <div className="col-10 col-md-8 col-lg-6">
+<form className="mt-5" method="POST" action="/recipes">
+  <div className="form-group">
+    <label for="Title">Title</label>
+    <input type="text" className="form-control" name="Title"/>
+  </div>
+  <div className="form-group">
+    <label for="ingredients">Ingredients</label>
+    <textarea className="form-control" name="ingredients" rows="3"></textarea>
+  </div>
+  <div className="form-group">
+    <label for="instructions">Instructions</label>
+    <textarea className="form-control" name="instructions" rows="3"></textarea>
+  </div>
+    <button type="submit" className="btn btn-primary btn-customized">Add</button>
+</form>
+        </div>
+    </div>
+</div>
+ </DefaultLayout>
+    );
+  }
+}
+
+module.exports = createPage;
