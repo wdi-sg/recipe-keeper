1a926d82adbcf514083e5ff56f1297ebd7524e98
Robert Kolsek <kolsek.income@gmail.com>
2020-01-17 20:45:58 +0800

    MVP done

--- index.js
+++ index.js
@@ -1,27 +1,186 @@
 const jsonfile = require('jsonfile');
-
-const file = 'data.json';
-
 const express = require('express');
-
 const app = express();
+const methodOverride = require('method-override')
+const reactEngine = require('express-react-views').createEngine();
 
-app.use(express.static(__dirname+'/public/'));
+//JSON
+const file = 'recipes.json';
 
+//INIT
+app.use(express.static(__dirname + '/public/'));
 app.use(express.json());
-
 app.use(express.urlencoded({
   extended: true
 }));
-
-const methodOverride = require('method-override')
-
 app.use(methodOverride('_method'));
 
-const reactEngine = require('express-react-views').createEngine();
-
+//REACT ENGINE
 app.engine('jsx', reactEngine);
-
 app.set('views', __dirname + '/views');
-
 app.set('view engine', 'jsx');
+
+// ******************************************
+// ******************************************
+//             BOILERPLATE END
+// ******************************************
+// ******************************************
+
+app.get('/recipes/new', (request, response) => {
+  //render new recipe form
+  response.render('new')
+})
+
+app.post('/recipes', (request, response) => {
+  //get newRecipe from post request form
+  const newRecipe = request.body
+
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    //push recipe into json file
+    obj.recipes.push(newRecipe)
+    //retrieve pushed recipe's ID
+    const newRecipeID = obj.recipes.length - 1
+    //create data obj with the new item, for rendering success message
+    const data = {
+      recipe: obj.recipes[newRecipeID],
+      success: "Item added successfully!"
+    }
+
+    jsonfile.writeFile(file, obj, (err) => {
+      if (err) {
+        return console.log(err)
+      }
+      response.render('displayid', data)
+    })
+    //END readFile
+  })
+  //END app.post
+})
+
+app.get('/recipes/:id', (request, response) => {
+  //get id
+  const id = request.params.id
+
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    //get recipe object by id
+    const requestedRecipe = obj.recipes[id]
+
+    //store recipe object in data object
+    const data = {
+      recipe: requestedRecipe
+    }
+
+    //render display .jsx and pass recipe as data
+    response.render('displayid', data)
+    //END readFile
+  })
+  //END app.get
+})
+
+app.get('/recipes/:id/edit', (request, response) => {
+  //get id
+  const id = request.params.id
+
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      return console.log(err)
+    }
+    //requestedObj content set to object in recipe book
+    const requestedRecipe = obj.recipes[id]
+    //create data object to pass to edit form
+    const data = {
+      id: id,
+      recipe: requestedRecipe
+    }
+
+    response.render('edit', data)
+    //END readFile
+  })
+  //END app.get
+})
+
+app.put('/recipes/:id', (request, response) => {
+  //get id
+  const id = request.params.id
+  //store submitted change 
+  const editedRecipe = request.body
+
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    //copy editedRecipe into recipe object
+    Object.assign(obj.recipes[id], editedRecipe)
+    //create data object for rendering success message
+    const data = {
+      success: "Edited successfully!",
+      recipe: editedRecipe
+    }
+
+    jsonfile.writeFile(file, obj, (err) => {
+      if (err) {
+        return console.log(err)
+      }
+      response.render('displayid', data)
+    })
+    //END readFile
+  })
+  //END app.put
+})
+
+app.get('/recipes/:id/delete', (request, response) => {
+  //get id
+  const id = request.params.id
+
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      return console.log(err)
+    }
+    //requestedObj content set to object in recipe book
+    const requestedRecipe = obj.recipes[id]
+    //create data object to pass to delete form
+    const data = {
+      id: id,
+      recipe: requestedRecipe
+    }
+
+    response.render('delete', data)
+    //END readFile
+  })
+  //END app.get
+})
+
+app.delete('/recipes/:id', (request, response) => {
+  // get id
+  const id = request.params.id
+
+  jsonfile.readFile(file, (err, obj) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    //remove Object at id
+    obj.recipes.splice(id, 1)
+
+    jsonfile.writeFile(file, obj, (err) => {
+      if (err) {
+        return console.log(err)
+      }
+      
+      response.send("Deleted!")
+    })
+    //END readFile
+  })
+  //END app.delete
+})
+
+app.listen(3000, () => console.log("Listening.....TO YOUR INPUT"))
---
+++ views/delete.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+
+class EditedRecipe extends React.Component {
+render() {
+
+    const id = this.props.id
+    const actionURL = "/recipes/"+id+"?_method=delete"
+
+return (
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <form action={actionURL} method="POST">
+        <input type="text" placeholder="title" name="title" value={this.props.recipe.title} readOnly/>
+        <input type="text" placeholder="ingredients" name="ingredients" value={this.props.recipe.ingredients} readOnly/>
+        <textarea type="text" placeholder="description" rows="5" cols="30" name="description" value={this.props.recipe.description} readOnly/>
+        <input type="submit" value="Submit!"/>
+    </form>
+</body>
+</html>
+);
+}
+}
+
+module.exports = EditedRecipe;
