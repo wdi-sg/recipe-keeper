70c0e95fa3d73535fc7a1d3ea2d31026d23a4521
Stuart Myers <stuart.myers@gmail.com>
2020-01-17 15:13:50 +0800

    Added boilerplate from readme

---
+++ index.js
@@ -0,0 +1,27 @@
+const jsonfile = require('jsonfile');
+
+const file = 'data.json';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const methodOverride = require('method-override')
+
+app.use(methodOverride('_method'));
+
+const reactEngine = require('express-react-views').createEngine();
+
+app.engine('jsx', reactEngine);
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'jsx');
