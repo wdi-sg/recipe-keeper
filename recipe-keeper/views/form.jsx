872491a6aaeab9a1878afa70118aacdc5bdbfcbb
upieez <samuelhww@gmail.com>
2020-01-19 19:39:01 +0800

    add form page with style and format

---
+++ recipe-keeper/views/form.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+
+var Layout = require('./layout')
+
+class Form extends React.Component {
+  render() {
+    return (
+        <Layout>
+          <div className="container">
+          <h1>Add Recipes</h1>
+
+            <form method="POST" action="/recipes">
+
+            <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control" name="title"/>
+            </div>
+
+            <div className="form-group">
+            <label>Ingredients</label>
+            <textarea className="form-control" name="ingredients"></textarea>
+            </div>
+
+            <div className="form-group">
+            <label>Instructions</label>
+            <textarea className="form-control" name="instructions"></textarea>
+            </div>
+
+            <button type="submit" className="btn btn-danger btn-customized">Send me</button>
+
+            </form>
+          </div>
+          </Layout>
+    );
+  }
+}
+
+module.exports = Form;
