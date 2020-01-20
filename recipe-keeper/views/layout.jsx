7847f9f9c1aed979f6c2f9694de27a2803e9d574
upieez <samuelhww@gmail.com>
2020-01-20 09:17:21 +0800

    add link to homepage on clicking logo

--- recipe-keeper/views/layout.jsx
+++ recipe-keeper/views/layout.jsx
@@ -22,7 +22,7 @@ class Layout extends React.Component {
             <title>Recipe book!</title>
             </head>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <a className="navbar-brand" href="#">Recipe Keeper</a>
+              <a className="navbar-brand" href="/recipes/">Recipe Keeper</a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
