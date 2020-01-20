3f33d5647ef526e260c2718eb42366a3063620de
Stuart Myers <stuart.myers@gmail.com>
2020-01-20 09:51:43 +0800

    beautify

--- views/components/multilinestringtolist.jsx
+++ views/components/multilinestringtolist.jsx
@@ -4,31 +4,31 @@ const PropTypes = require('prop-types');
 class MultiLineStringToList extends React.Component {
     render() {
 
-      //
-      if (!this.props.string) return (null);
-      let string = this.props.string;
-      let stringArray = string.split('\n');
+        //
+        if (!this.props.string)
+            return (null);
+        let string = this.props.string;
+        let stringArray = string.split('\n');
 
-      // create a list, it's easier to make a list than to have line breaks.
-      const StringList = stringArray.map((value, index) => {
-        return (<li key={index}>{value}</li>);
-      })
+        // create a list, it's easier to make a list than to have line breaks.
+        const StringList = stringArray.map((value, index) => {
+            return (<li key={index}>{value}</li>);
+        })
 
-      // If we specify we want an ordered list
-      if (this.props.isOrdered) {
-        return (
-          <ol>
-            {StringList}
-          </ol>
-        )
+        // If we specify we want an ordered list
+        if (this.props.isOrdered) {
+            return (
+            <ol>
+                {StringList}
+            </ol>)
 
-        // Otherwise return an unordered list.
-      } else {
-      return (
-        <ul>
-          {StringList}
-        </ul>
-      )}
+            // Otherwise return an unordered list.
+        } else {
+            return (
+            <ul>
+                {StringList}
+            </ul>)
+        }
     }
 };
 
