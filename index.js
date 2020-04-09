172fa4af639868c29f130a5de522744acf906a62
mdazharaw <darkaz62@hotmail.com>
2020-04-10 07:39:41 +0800

    Updated form layout

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-const express = require('express')
+const jsonfile = require('jsonfile');
+
+const file = 'data.json';
+
+const express = require('express');
+
+const popup = require('popups');
+
 const app = express();
 
+app.use(express.static(__dirname+'/public/'));
 
-// this line below, sets a layout look to your express project
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const methodOverride = require('method-override')
+
+app.use(methodOverride('_method'));
+
 const reactEngine = require('express-react-views').createEngine();
+
 app.engine('jsx', reactEngine);
 
-// this tells express where to look for the view files
 app.set('views', __dirname + '/views');
 
-// this line sets react to be the default view  engine
 app.set('view engine', 'jsx');
 
 app.get('/', (req, res) => {
   // running this will let express to run home.handlebars file in your views folder
   res.render('add-recipe')
 })
+
+app.post('/add-recipe', function(req, res) {
+    //debug code (output request body)
+   console.log("New Recipe Added");
+   
+    res.render('add-recipe');
+  });
 
 app.listen(3000, ()=>{
     console.log("Starting cookbook...");
--- views/add-recipe.jsx
+++ views/add-recipe.jsx
@@ -3,29 +3,34 @@ var React = require('react');
 
 class AddRecipe extends React.Component {
     render() {
+       
+
+        
         return (
             <html>
                 <head>
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                 </head>
                 <body>
-                    <div className="container w-75">
+                    <div className="container mw-50 w-50">
                         <h1 className="text-center font-italic"><u><strong>Add New Recipe</strong></u></h1>
-                        <form className="w-100 d-flex-column justify-content-center">
-                            <div className="form-group">
-                                <label for="title" className="font-weight-bold">Recipe Title:</label>
-                                <textarea className="form-control" rows="1" id="title"></textarea>
-                            </div>
-                            <div className="form-group">
-                                <label for="instructions" className="font-weight-bold">Instructions:</label>
-                                <textarea className="form-control" rows="3" id="instructions"></textarea>
-                            </div>
-                            <div className="form-group">
-                                <label for="ingredients" className="font-weight-bold">Ingredients:</label>
-                                <textarea className="form-control" rows="3" id="ingredients"></textarea>
+
+                        <form className="w-100 text-center" method="POST" action="/add-recipe">
+
+                            <div className="form-group text-left">
+                                <label htmlFor="title" className="font-weight-bold">Title:</label>
+                                <textarea className="form-control" rows="1" name="title"></textarea>
                             </div>
 
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <div className="form-group text-left">
+                                <label htmlFor="ingredients" className="font-weight-bold">Ingredients:</label>
+                                <textarea className="form-control" rows="3" name="ingredients"></textarea>
+                            </div>
+                            <div className="form-group text-left">
+                                <label htmlFor="instructions" className="font-weight-bold">Instructions:</label>
+                                <textarea className="form-control" rows="3" name="instructions"></textarea>
+                            </div>
+                            <button type="submit" className="btn btn-dark  w-50">Submit</button>
                         </form>
                     </div>
                 </body>
