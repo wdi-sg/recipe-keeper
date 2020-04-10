5aa1919f39578e4376529160460ff536228c1b92
travisenquiry <travisenquiry@gmail.com>
2020-04-10 18:31:13 +0800

    basic boilerplate completed

---
+++ index.js
@@ -0,0 +1,23 @@
+const jsonfile = require('jsonfile');
+const file = 'data.json';
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/public/'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'));
+const reactEngine = require('express-react-views').createEngine();
+app.engine('jsx', reactEngine);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+
+//all recipe index
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.listen(3000);
