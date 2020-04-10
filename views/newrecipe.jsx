e89a0bc117fb074561ce31c3b064f7e05197bddb
Dyana Wu <dyanawu@gmail.com>
2020-04-11 02:40:23 +0800

    Tweak form styles

--- views/newrecipe.jsx
+++ views/newrecipe.jsx
@@ -1,4 +1,4 @@
-var React = require('react');
+const React = require('react');
 
 const Ingredients = (obj) => {
   let list = obj.list;
@@ -10,7 +10,7 @@ const Ingredients = (obj) => {
 
         return (
           <React.Fragment key={item.id}>
-            <div className="form-group row">
+            <div className="form-group form-row">
               <div className="col">
                 <input className="ingredient form-control"
                        name={igdname}
@@ -41,54 +41,63 @@ class NewRecipe extends React.Component {
       <html>
         <head>
           <link rel="stylesheet"
-                href="../files/bootstrap.min.css"
+                href="../static/bootstrap.min.css"
                 />
-          <script src="../files/form.js" />
+          <script src="../static/form.js" />
         </head>
 
         <body>
           <div className="container">
-            <h1> Add a Recipe!</h1>
-            <form method="POST" action="/recipes" id="newrecipe">
-              <div className="form-group row">
-                <div className="col">
-                  <label className="h4" htmlFor="recipename">Recipe Name</label>
-                  <input className="form-control"
-                         name="recipename"
-                         id="recipename"
-                         defaultValue={recipeName}/>
-                </div>
+            <div className="row">
+              <div className="col">
+                <h1> Add a Recipe!</h1>
               </div>
+            </div>
 
-              <div id="ingredientlist">
-                <div className="form-group row">
-                  <div className="col">
-                    <label className="h4">Ingredient</label>
+            <div className="row">
+              <div className="col">
+                <form method="POST" action="/recipes" id="newrecipe">
+                  <div className="form-group row">
+                    <div className="col">
+                      <label className="h4" htmlFor="recipename">Recipe Name</label>
+                      <input className="form-control"
+                             name="recipename"
+                             id="recipename"
+                             defaultValue={recipeName}/>
+                    </div>
                   </div>
-                  <div className="col">
-                    <label className="h4">Quantity</label>
+
+                  <div id="ingredientlist" className="form-group">
+                    <div className="form-row">
+                      <div className="col">
+                        <label className="h4">Ingredient</label>
+                      </div>
+                      <div className="col">
+                        <label className="h4">Quantity</label>
+                      </div>
+                    </div>
+                    <Ingredients list={this.props.recipe.ingredients}/>
+                    <div className="row">
+                      <div className="col">
+                        <button className="btn btn-secondary btn-block" id="increase">Add More Ingredients</button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <Ingredients list={this.props.recipe.ingredients}/>
-                <div className="row">
-                  <div className="col">
-                    <button id="increase">Add More Ingredients</button>
+
+                  <div className="form-group row">
+                    <div className="col">
+                      <label className="h4" htmlFor="instructions">Instructions</label>
+                      <textarea className="form-control"
+                                name="instructions"
+                                id="instructions"
+                                defaultValue={recipeInst}/>
+                    </div>
                   </div>
-                </div>
+
+                  <button type="submit" className="btn btn-primary btn-lg btn-block">Add Recipe</button>
+                </form>
               </div>
-
-              <div className="form-group row">
-                <div className="col">
-                  <label className="h4" htmlFor="instructions">Instructions</label>
-                  <textarea className="form-control"
-                            name="instructions"
-                            id="instructions"
-                            defaultValue={recipeInst}/>
-                </div>
-              </div>
-
-              <button type="submit">Add Recipe</button>
-            </form>
+            </div>
 
           </div>
         </body>
