009676bd3c2bb784459a0215d7afc3130c2a9bef
Dyana Wu <dyanawu@gmail.com>
2020-04-11 00:29:58 +0800

    Add main page template scaffold

---
+++ views/recipelist.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+
+class filename extends React.Component {
+  render() {
+    return (
+      <html>
+        <head>
+          <link rel="stylesheet"
+                href="../files/bootstrap.min.css"
+                />
+        </head>
+
+        <body>
+          <div className="container">
+            
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = filename;
