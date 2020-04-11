49a3140357746c0a30a992740462aea6953a5636
Dyana Wu <dyanawu@gmail.com>
2020-04-11 19:20:13 +0800

    Scaffold to view single recipe

--- views/recipeview.jsx
+++ views/recipeview.jsx
@@ -1,9 +1,29 @@
 const React = require('react');
 
+// recipe
+// {
+//   name: 'SEI Homework',
+//   ingredients: {
+//     '1': { ing: 'Iced coffee', qty: '3 cups' },
+//     '2': { ing: 'Spotify', qty: '1' },
+//     '3': { ing: 'console.log()', qty: "really a lot more than you'd think" },
+//     '4': { ing: 'StackOverflow', qty: 'as necessary' }
+//   },
+//   instructions: '1. Make coffee\r\n' +
+//     '2. Get some music going\r\n' +
+//     '3. Code\r\n' +
+//     '4. Debug\r\n' +
+//     '5. Repeat until done.'
+// }
+
+// edit link
+// delete link
+
 class RecipeView extends React.Component {
   render() {
+    let id = this.props.id
     let recipe = this.props.recipe;
-    console.log(recipe);
+    console.log("view", id, recipe);
     return (
       <html>
         <head>
@@ -14,7 +34,6 @@ class RecipeView extends React.Component {
 
         <body>
           <div className="container">
-            {`IT'S A SINGLE RECIPE`}
           </div>
         </body>
       </html>
