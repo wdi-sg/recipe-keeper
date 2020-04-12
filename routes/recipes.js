179556402f72d2a6983db2e49a66a59bbdcebe04
kenan <xinkenan.11@hotmail.com>
2020-04-13 00:32:24 +0800

    update

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,6 +2,12 @@ const router = require('express').Router()
 const Recipe = require('../models/recipe.model')
 const Ingredient = require('../models/ingredient.model')
 
+router.route('/').get((req, res) => {
+  Recipe.findAll()
+    .then(jsonArr => res.json(jsonArr))
+    .then(err => res.status(400).json('Error: ' + err))
+})
+
 router.route('/add').post((req, res) => {
   const { name, ingredients, instructions } = req.body
   const newRecipe = new Recipe(name, instructions)
