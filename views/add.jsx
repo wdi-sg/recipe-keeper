fd9486609d87cacd1c1d308a2870fd566b6cc23d
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-04-12 12:18:57 +0800

    1st JSX files commit

---
+++ views/add.jsx
@@ -0,0 +1,44 @@
+const React = require('react');
+
+class Add extends React.Component {
+    render() {
+
+
+        return (
+            <html>
+            <head>
+            <link rel="stylesheet" href="/style.css"/>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous"/>
+            </head>
+            <body>
+            <div className="add-page">
+
+            <ul class="nav justify-content-center nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link active" href="/recipe">Home</a>
+              </li>
+              </ul>
+
+            <form method="POST" action="/recipe">
+            <div class="form-group">
+                <label for="exampleFormControlInput1 add-title">Title</label>
+                <input class="form-control form-control-lg" id="exampleFormControlInput1" type="text" name="title"/>
+                <label for="exampleFormControlInput1 add-title">Ingredients</label>
+                <input class="form-control form-control-lg" id="exampleFormControlInput1" type="text" name="ingredients"/>
+                <label for="exampleFormControlInput1 add-title">Instructions</label>
+                <input class="form-control form-control-lg" id="exampleFormControlInput1" type="text" name="instructions"/>
+                <button type="submit" class="btn btn-light btn-lg del-button">Submit</button>
+          </div>
+                </form>
+
+            </div>
+            </body>
+            </html>
+
+
+
+            )
+    }
+}
+
+module.exports = Add;
